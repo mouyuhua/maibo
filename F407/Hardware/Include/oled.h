424342f4c6748360fffcba66488dbe1f4c6f6589
mouyuhua <mouyuhua1234@163.com>
2022-02-15 16:11:11 +0800

    OLED驱动

--- F407/Hardware/Include/oled.h
+++ F407/Hardware/Include/oled.h
@@ -1,73 +1,96 @@
-//////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//  文 件 名   : main.c
-//  版 本 号   : v2.0
-//  作    者   : Evk123
-//  生成日期   : 2014-0101
-//  最近修改   : 
-//  功能描述   : 0.69寸OLED 接口演示例程(STM32F103ZE系列IIC)
-//              说明: 
-//              ----------------------------------------------------------------
-//              GND   电源地
-//              VCC   接5V或3.3v电源
-//              SCL   接PD6（SCL）
-//              SDA   接PD7（SDA）            
-//              ----------------------------------------------------------------
-//All rights reserved
-//////////////////////////////////////////////////////////////////////////////////
 #ifndef __OLED_H
 #define __OLED_H			  	 
 #include "sys.h"
-#include "stdlib.h"	    	
-#define OLED_MODE 0
-#define SIZE 8
-#define XLevelL		0x00
-#define XLevelH		0x10
-#define Max_Column	128
-#define Max_Row		64
-#define	Brightness	0xFF 
-#define X_WIDTH 	128
-#define Y_WIDTH 	64	    						  
-//-----------------OLED IIC端口定义----------------  					   
+    	
+//定义输出扫描方向
+#define OLED_RL 1           //从右向左
+#define OLED_UD 0           //从上到下
 
+//颜色反转
+#define OLED_INVERSE 1
+
+//使用字体
+#define USE_FONT 0
+/**
+ * 从外部引入I2C设置
+ * 外部I2C文件需要有以下宏定义和函数：
+ *      宏定义：
+ *          OLED_SCLK_Clr()
+ *          OLED_SCLK_Set()
+ *          OLED_SDIN_Clr()
+ *          OLED_SDIN_Set()
+ *      函数：
+ *          void IIC_Start(void);
+ *          void IIC_Stop(void);
+ *          void IIC_Wait_Ack(void);
+ *					void IIC_Send_Byte(u8 IIC_Byte);
+ */
+#define USE_MY_I2C 1
+
+/**
+ * 内部I2C定义
+ */
+#if USE_MY_I2C
+//导入外部I2C
+#include "myiic.h"
+
+#else
+
+//内部I2C设置
 #define OLED_SCLK_Clr() GPIO_ResetBits(GPIOA,GPIO_Pin_5)//SCL
 #define OLED_SCLK_Set() GPIO_SetBits(GPIOA,GPIO_Pin_5)
 
 #define OLED_SDIN_Clr() GPIO_ResetBits(GPIOA,GPIO_Pin_7)//SDA
 #define OLED_SDIN_Set() GPIO_SetBits(GPIOA,GPIO_Pin_7)
 
- 		     
+void IIC_Start(void);
+void IIC_Stop(void);
+void IIC_Wait_Ack(void);
+void IIC_Send_Byte(u8 IIC_Byte);
+
+#endif
+
+
 #define OLED_CMD  0	//写命令
 #define OLED_DATA 1	//写数据
 
-
 //OLED控制用函数
-void OLED_WR_Byte(unsigned dat,unsigned cmd);  
-void OLED_Display_On(void);
-void OLED_Display_Off(void);	   							   		    
 void OLED_Init(void);
+
+void Write_IIC_Command(u8 Command);
+
+void Write_IIC_Data(u8 Data);
+
+void OLED_WR_Byte(u8 dat, u8 cmd);
+
+void OLED_Refresh(void);
+
+void OLED_DrawPoint(u8 x, u8 y, u8 t);
+
+void OLED_Display_On(void);
+
+void OLED_Display_Off(void);
+
+void OLED_On(void);
+
 void OLED_Clear(void);
-void OLED_DrawPoint(u8 x,u8 y,u8 t);
-void OLED_Fill(u8 x1,u8 y1,u8 x2,u8 y2,u8 dot);
-void OLED_ShowChar(u8 x,u8 y,u8 chr,u8 Char_Size);
-void OLED_ShowNum(u8 x,u8 y,u32 num,u8 len,u8 size);
-void OLED_ShowString(u8 x,u8 y, u8 *p,u8 Char_Size);	 
-void OLED_Set_Pos(unsigned char x, unsigned char y);
-void OLED_ShowCHinese(u8 x,u8 y,u8 no);
-void OLED_DrawBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1,unsigned char BMP[]);
-void Delay_50ms(unsigned int Del_50ms);
-void Delay_1ms(unsigned int Del_1ms);
-void fill_picture(unsigned char fill_Data);
-void Picture();
-void IIC_Start();
-void IIC_Stop();
-void Write_IIC_Command(unsigned char IIC_Command);
-void Write_IIC_Data(unsigned char IIC_Data);
-void Write_IIC_Byte(unsigned char IIC_Byte);
 
-void IIC_Wait_Ack();
+u32 oled_pow(u8 m, u8 n);
+
+void OLED_Draw(u8 x0, u8 y0, u8 x1, u8 y1, u8 BMP[]);
+
+void OLED_Fill(u8 x1, u8 y1, u8 x2, u8 y2, u8 dot[]);
+
+#if USE_FONT
+
+void OLED_ShowChar(u8 x, u8 y, u8 chr, u8 Char_Size);
+
+void OLED_ShowString(u8 x, u8 y, u8* chr, u8 Char_Size);
+
+void OLED_ShowChinese(u8 x, u8 y, u8 no);
+
+void OLED_ShowNum(u8 x, u8 y, u32 num, u8 len, u8 size);
+
+#endif
+
 #endif  
-	 
-
-
-
